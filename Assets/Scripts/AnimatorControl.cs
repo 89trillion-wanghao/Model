96c28f94db5aba994e39ad9c28aca2debe30bf57
89trillion-wanghao <wanghao@89trillion.com>
2021-04-02 10:04:01 +0800

    add .gitignore

---
+++ Assets/Scripts/AnimatorControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+/// <summary>
+/// 动画控制脚本
+/// </summary>
+public class AnimatorControl : MonoBehaviour
+{
+    private Animator infantryAnimator;
+    void Start()
+    {
+        infantryAnimator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        // 三种动画状态之间切换
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            infantryAnimator.SetTrigger("ToStand");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            infantryAnimator.SetTrigger("ToRun");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            infantryAnimator.SetTrigger("ToAttack");
+        }
+
+        
+        // AnyState切换到各个动画状态
+/*   
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            infantryAnimator.SetInteger("ChangeInt",1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            infantryAnimator.SetInteger("ChangeInt",2);
+        }      
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            infantryAnimator.SetInteger("ChangeInt",3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            infantryAnimator.SetInteger("ChangeInt",0);
+        }
+*/    
+    }
+}
